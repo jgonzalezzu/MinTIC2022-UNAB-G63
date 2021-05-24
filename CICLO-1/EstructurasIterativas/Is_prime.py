a9b7ef1e372dc901dea142ed7a5da1afc1f5d31b
Julian Gonzalez <jegonzalezzu@unal.edu.co>
2021-05-23 22:44:45 -0500

    Modificacion README con las actividades y capetas

---
+++ CICLO-1/EstructurasIterativas/Is_prime.py
@@ -0,0 +1,7 @@
+#TODO:Usar For in
+'''
+Identificar si el número es primo o no, usando instrucciones iterativas.
+Tenemos que crear un programa que nos permita saber si el número es o
+no primo. Recordemos, que un número primo es aquel número mayor a
+uno que solo es divisible en uno y en sí mismo
+'''
